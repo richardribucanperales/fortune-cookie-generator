f44e7afb86d925f0167e7973ce8b4de216292be8
gawigwen <19rodnce@waipahuh.k12.hi.us>
2018-02-14 14:37:51 -1000

    html and css is almost there

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var fortunesList = ["you are a god, go slay people today", "today you will find yourself ded"]
+
+function generateFortuneCookie(){
+  for (i=0;i=<fortunesList.length;i++){
+    
+  }
+}
