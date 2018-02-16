81118ddfb077aa8a2a96b15a1ea83b2b62a45835
gawigwen <19rodnce@waipahuh.k12.hi.us>
2018-02-16 13:04:41 -1000

    almost

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+console.log("loaded")
 var fortunesList = ["you are a god, go slay people today", "today you will find yourself ded"]
-
-function generateFortuneCookie(){
-  for (i=0;i=<fortunesList.length;i++){
-    
-  }
+function generateFortuneCookie () {
+  var fortunesList = ['the gods are angry'] ;
+  /* all possible fortunes */
+  var random = fortunesList[Math.floor(Math.random() * fortunesList.length)];
+  document.getElementById('text').textContent = random;
+  /* randomizer set to random variable, change textContent of fortune-cookie-text to the random variable value */
+  console.log(random)
+  var list = document.getElementById('prevFortune');
+  var node = document.createElement("div");
+  node.innerHTML = random
+  list.appendChild(node);   
 }
