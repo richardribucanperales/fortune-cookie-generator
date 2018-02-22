b8700c11b7447a6fd61b1ea1f15fd7e59df6976b
gawigwen <19rodnce@waipahuh.k12.hi.us>
2018-02-21 14:37:08 -1000

    done kinda

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 console.log("it is loaded")
 
-var theList = ["we're gods","its kinda too late","do it","sexy keegan is watching","have fun, you're gonna die soon anyawys","the ice aint gonna pick itself up","You are alone, child. There is only darkness for you, and only death for your people. These ancients are just the beginning. I will command a great and terrible army, and we will sail to a billion worlds. We will sail until every light has been extinguished. You are strong, child, but I am beyond strength. I am the end, and I have come for you"]
+var theList = ["we're gods","its kinda too late","do it","sexy keegan is watching","have fun, you're gonna die soon anyawys","so its not gonna be easy its gonna be really hard. and we're gonna have to work at this every day but i wanna do that because i want you. i want all of you. forever. you and me. everyday. will you do something for me? please? just picture your life for me. 30 years from now, 40 years from now? what's it look like?","You are alone, child. There is only darkness for you, and only death for your people. These ancients are just the beginning. I will command a great and terrible army, and we will sail to a billion worlds. We will sail until every light has been extinguished. You are strong, child, but I am beyond strength. I am the end, and I have come for you"]
 var pastFortunes = [];
 var buttonList = ["another one", "another? really?", "don't you think you had enough?", "lmao press me"]
 function generateFortuneCookie(){
