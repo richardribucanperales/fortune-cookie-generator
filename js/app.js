a6b8399028f51b85907154de4aa90caf7c994b11
gawigwen <19rodnce@waipahuh.k12.hi.us>
2018-02-23 14:39:32 -1000

    done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ console.log("it is loaded")
 var theList = ["we're gods","its kinda too late","do it","sexy keegan is watching","have fun, you're gonna die soon anyawys","so its not gonna be easy its gonna be really hard. and we're gonna have to work at this every day but i wanna do that because i want you. i want all of you. forever. you and me. everyday. will you do something for me? please? just picture your life for me. 30 years from now, 40 years from now? what's it look like?","You are alone, child. There is only darkness for you, and only death for your people. These ancients are just the beginning. I will command a great and terrible army, and we will sail to a billion worlds. We will sail until every light has been extinguished. You are strong, child, but I am beyond strength. I am the end, and I have come for you"]
 var pastFortunes = [];
 var buttonList = ["another one", "another? really?", "don't you think you had enough?", "lmao press me"]
+var number = 0
+var fortune = theList[a]
 function generateFortuneCookie(){
   /*math random crap*/
 
@@ -12,21 +14,43 @@ function generateFortuneCookie(){
   document.getElementById("fortune-onclick").innerHTML = fortune
   var b = Math.floor(Math.random()*4)
   var button = buttonList[b]
-  document.getElementById("fortune-button").innerHTML = button
+  document.getElementById("button1").innerHTML = button
 
   /*the append crap*/
 
   var div = document.createElement("div")
   div.innerHTML = fortune
   document.getElementById("previous-fortunes").appendChild(div)
-  var div1 = document.createElement("div1")
+  var div1 = document.createElement("div")
   div1.innerHTML = "----------"
   document.getElementById("previous-fortunes").appendChild(div1)
-  if (fortune = pastFortunes[0]){
-    console.log(fortune)
-    fortune = theList[a+1]
-  }else if (pastFortunes[1] = pastFortunes[0]){
-    console.log(fortune)
-    fortune = theList[a+1]
-  }
+
+  /*add number*/
+  document.getElementById("fortune-number2").innerHTML = number += 1
+}
+
+function generateFortuneMax(){
+  /*generate math randoms*/
+  var c = Math.floor(Math.random()*7)
+  var d = Math.floor(Math.random()*7)
+  var e = Math.floor(Math.random()*7)
+  var f = Math.floor(Math.random()*7)
+  var g = Math.floor(Math.random()*7)
+
+  /*the fortunes*/
+  var fortunes = [theList[c], "||", theList[d], "||", theList[e], "||", theList[f], "||", theList[g]]
+  document.getElementById("fortune-onclick").innerHTML = fortunes
+
+  /*appending fortunes*/
+  var div2 = document.createElement("div")
+  div2.innerHTML = fortunes
+  document.getElementById("previous-fortunes").appendChild(div2)
+  var div3 = document.createElement("div")
+  div3.innerHTML = "----------"
+  document.getElementById("previous-fortunes").appendChild(div3)
+  document.getElementById("fortune-number2").innerHTML = number += 5
+  /*wacky buttons*/
+  var c = Math.floor(Math.random()*4)
+  var button1 = buttonList[c]
+  document.getElementById("button2").innerHTML = button1
 }
